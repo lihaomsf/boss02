c4cea748e8fade05a46beb3f449daf3f0957e17f
root <694887790@qq.com>
2018-03-15 21:25:23 +0800

    快递标准列表显示

---
+++ bos_management/bos_management_dao/src/main/java/com/itheima/bos/dao/base/StandardRepository.java
@@ -0,0 +1,18 @@
+package com.itheima.bos.dao.base;
+
+import java.io.Serializable;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.itheima.bos.domain.base.Standard;
+
+/**  
+ * ClassName:StandardRepository <br/>  
+ * Function:  <br/>  
+ * Date:     2018年3月14日 下午9:12:12 <br/>       
+ */
+public interface StandardRepository extends JpaRepository<Standard, Long>{
+
+
+}
+  
