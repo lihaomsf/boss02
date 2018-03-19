986fd3fe136144754888c481639784edb7eab8d0
root <694887790@qq.com>
2018-03-19 13:06:28 +0800

    代码重构

--- bos_management/bos_management_web/src/main/java/com/itheima/bos/web/action/base/AreaAction.java
+++ bos_management/bos_management_web/src/main/java/com/itheima/bos/web/action/base/AreaAction.java
@@ -4,16 +4,11 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletResponse;
 
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Row;
-import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Namespace;
 import org.apache.struts2.convention.annotation.ParentPackage;
@@ -26,14 +21,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 
 import com.itheima.bos.domain.base.Area;
-import com.itheima.bos.domain.base.Courier;
-import com.itheima.bos.domain.base.Standard;
 import com.itheima.bos.service.base.AreaService;
+import com.itheima.bos.web.action.CommonAction;
 import com.itheima.utils.PinYin4jUtils.PinYin4jUtils;
-import com.opensymphony.xwork2.ActionSupport;
-import com.opensymphony.xwork2.ModelDriven;
 
-import net.sf.json.JSONObject;
 import net.sf.json.JsonConfig;
 
 /**
@@ -47,15 +38,12 @@ import net.sf.json.JsonConfig;
 @ParentPackage("struts-default")
 @Controller
 @Scope("prototype")
-public class AreaAction extends ActionSupport implements ModelDriven<Area> {
-    
-    private Area model = new Area();
-    
-    @Override
-    public Area getModel() {
-          
-        return model;
+public class AreaAction extends CommonAction<Area> {
+
+    public AreaAction() {
+        super(Area.class);
     }
+
     @Autowired
     private AreaService areaService;
 
@@ -69,35 +57,36 @@ public class AreaAction extends ActionSupport implements ModelDriven<Area> {
     @Action(value = "areaAction_importXLS", results = {@Result(name = "success",
             location = "/pages/base/area.html", type = "redirect")})
     public String importXLS() {
-        // 用来保存数据的集合
-        ArrayList<Area> list = new ArrayList<>();
 
         try {
-            HSSFWorkbook workbook = new HSSFWorkbook(new FileInputStream(file));
-            // 读取工作簿
-            HSSFSheet sheet = workbook.getSheetAt(0);
-            // 遍历行
+            HSSFWorkbook hssfWorkbook =
+                    new HSSFWorkbook(new FileInputStream(file));
+            // 读取第一个工作簿
+            HSSFSheet sheet = hssfWorkbook.getSheetAt(0);
+            // 储存对象的集合
+            List<Area> list = new ArrayList<>();
             for (Row row : sheet) {
-                // 第一行是标题,这一行的数据不要
+                // 跳过第一行
                 if (row.getRowNum() == 0) {
                     continue;
                 }
-                // 读取表格的数据
+                // 读取数据
                 String province = row.getCell(1).getStringCellValue();
                 String city = row.getCell(2).getStringCellValue();
                 String district = row.getCell(3).getStringCellValue();
                 String postcode = row.getCell(4).getStringCellValue();
-
-                // 截掉省市区的最后一个字符
+                // 截掉最后一个字符
                 province = province.substring(0, province.length() - 1);
                 city = city.substring(0, city.length() - 1);
                 district = district.substring(0, district.length() - 1);
-                // 获取城市编码
-                String citycode = PinYin4jUtils.hanziToPinyin(city, "").toUpperCase();
-                String[] headByString = PinYin4jUtils.getHeadByString(province + city + district, true);
-                // 获取简码
-                String shortcode = PinYin4jUtils.stringArrayToString(headByString);
-                // 构造一个Area
+                // 获取城市编码和简码
+                String citycode =
+                        PinYin4jUtils.hanziToPinyin(city, "").toUpperCase();
+                String[] headByString = PinYin4jUtils
+                        .getHeadByString(province + city + district);
+                String shortcode =
+                        PinYin4jUtils.stringArrayToString(headByString);
+                // 封装数据
                 Area area = new Area();
                 area.setProvince(province);
                 area.setCity(city);
@@ -107,27 +96,18 @@ public class AreaAction extends ActionSupport implements ModelDriven<Area> {
                 area.setShortcode(shortcode);
                 // 添加到集合
                 list.add(area);
+
             }
-            // 一次性保存区域数据
+            // 执行保存
             areaService.save(list);
+
             // 释放资源
-            workbook.close();
+            hssfWorkbook.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
 
         return SUCCESS;
-    }
- // 使用属性驱动获取数据
-    private int page;// 第几页
-    private int rows;// 每一页显示多少条数据
-
-    public void setPage(int page) {
-        this.page = page;
-    }
-
-    public void setRows(int rows) {
-        this.rows = rows;
     }
 
     // AJAX请求不需要跳转页面
@@ -141,33 +121,12 @@ public class AreaAction extends ActionSupport implements ModelDriven<Area> {
         Pageable pageable = new PageRequest(page - 1, rows);
 
         Page<Area> page = areaService.findAll(pageable);
-
-        // 总数据条数
-        long total = page.getTotalElements();
-        // 当前页要实现的内容
-        List<Area> list = page.getContent();
-        // 封装数据
-        Map<String, Object> map = new HashMap<>();
-
-        map.put("total", total);
-        map.put("rows", list);
-
-        // JSONObject : 封装对象或map集合
-        // JSONArray : 数组,list集合
-        // 把对象转化为json字符串
-        
-        
         JsonConfig jsonConfig = new JsonConfig();
-        jsonConfig.setExcludes(new String[] { "subareas" });
+        jsonConfig.setExcludes(new String[] {"subareas"});
 
-        String json = JSONObject.fromObject(map,jsonConfig).toString();
-
-        HttpServletResponse response = ServletActionContext.getResponse();
-        response.setContentType("application/json;charset=UTF-8");
-        response.getWriter().write(json);
+        page2json(page, jsonConfig);
 
         return NONE;
     }
-   
 
 }
